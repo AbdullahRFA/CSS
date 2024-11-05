ada8c5df6784ecdec769a1449acdb6428f270814
AbdullahRFA <shakibrybmn@gmail.com>
2024-11-06 01:30:53 +0600

    add separate js file

---
+++ Typing_text_effect/app.js
@@ -0,0 +1,8 @@
+// typed.js
+
+var typed = new Typed('#auto_type', {
+    strings: ['Programer', 'Web Developer', 'UX-UI Designer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true // Set this to true for infinite looping
+});
